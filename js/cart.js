7d8bed3a34577bd83bf0fe6cbd3f0861eb728b6e
Dmitry Shumovsky <d.shumovsky@bk.ru>
2022-05-20 00:34:12 +0300

    first commit

---
+++ js/cart.js
@@ -0,0 +1,18 @@
+const cart = () => {
+    const cartBtn = document.getElementById('cart');
+    const cartModal = document.querySelector('.cart');
+    const closeCartBtn = document.querySelector('.cart-close');
+
+    const openCart = () => {
+        cartModal.style.display = 'flex'
+    }
+
+    const closeCart = () => {
+        cartModal.style.display = '';
+    }
+
+    cartBtn.addEventListener('click', openCart);
+    closeCartBtn.addEventListener('click', closeCart)
+}
+
+cart();
